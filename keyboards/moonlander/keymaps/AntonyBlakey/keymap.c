64efc40b76e8abfe6e1ab6cf2ed835ea9a2381b3
AntonyBlakey <antony.blakey@gmail.com>
2022-12-03 02:06:13 +1100

    no tab next to space

--- keyboards/moonlander/keymaps/AntonyBlakey/keymap.c
+++ keyboards/moonlander/keymaps/AntonyBlakey/keymap.c
@@ -22,7 +22,7 @@ enum keycodes {
 
 enum { TD_SCLN_COLN};
 
-#define TH_TAB MT(MOD_LSFT, KC_TAB)
+#define TH_SFT MT(MOD_LSFT, KC_SPC)
 #define TH_SPC LT(NAV, KC_SPC)
 #define TH_ENT LT(SYM, KC_ENT)
 #define TH_BSPC MT(MOD_LCTL, KC_BSPC)
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,          XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX, 
         XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,          XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_ESC,  XXXXXXX,
         XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  TD_SCLN, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, TH_TAB,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT,  TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, TH_SFT,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT,  TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX 
     ),
 
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LCMD, KC_MPLY, XXXXXXX,          XXXXXXX, TABL,    XXXXXXX, XXXXXXX, KC_TAB,  C_GRV,   XXXXXXX, 
         XXXXXXX, OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  G_SPC,   XXXXXXX,          XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, GS_P,    XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_HOME, KC_PGDN, KC_PGUP, KC_END,  G_P,     XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, TH_TAB,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT,  TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, TH_SFT,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT,  TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX 
     ),
 
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX,          XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, 
         XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,          XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
         XXXXXXX, KC_F11,  KC_F12,  KC_F13,  KC_F14, KC_F15,                              XXXXXXX, XXXXXXX, KC_COMM, KC_DOT,  KC_MINS, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, TH_TAB,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT,  TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, TH_SFT,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT,  TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
     ),
 };
@@ -83,7 +83,7 @@ bool is_oneshot_cancel_key(uint16_t keycode) {
 
 bool is_oneshot_ignored_key(uint16_t keycode) {
     switch (keycode) {
-        case TH_SPC: 
+        case TH_SFT: 
         case OS_SHFT:
         case OS_CTRL:
         case OS_ALT:
@@ -125,7 +125,7 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             int keycode = keymap_key_to_keycode(layer, (keypos_t){col, row});
             if (keycode > KC_TRNS) {
                 uint8_t index = g_led_config.matrix_co[row][col];
-                if (keycode == KC_CAPS || keycode == TH_TAB) {
+                if (keycode == KC_CAPS || keycode == TH_SPC) {
                     if (host_keyboard_led_state().caps_lock) {
                         rgb_matrix_set_color(index, RGB_MAGENTA);
                     } else {
