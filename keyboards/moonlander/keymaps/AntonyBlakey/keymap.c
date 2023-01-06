283985d9ea7898dd3624754602cbe5c2ac7f3582
AntonyBlakey <antony.blakey@gmail.com>
2023-01-06 20:16:31 +1100

    change return and backspace

--- keyboards/moonlander/keymaps/AntonyBlakey/keymap.c
+++ keyboards/moonlander/keymaps/AntonyBlakey/keymap.c
@@ -23,9 +23,9 @@ enum keycodes {
 enum { TD_SCLN_COLN};
 
 #define TH_SFT MT(MOD_LSFT, KC_SPC)
-#define TH_SPC LT(NAV, KC_SPC)
-#define TH_ENT LT(SYM, KC_ENT)
-#define TH_BSPC MT(MOD_LCTL, KC_BSPC)
+#define TH_NAV LT(NAV, KC_SPC)
+#define TH_SYM LT(SYM, KC_SPC)
+#define TH_CTL MT(MOD_LCTL, KC_SPC)
 
 #define TD_SCLN TD(TD_SCLN_COLN)
 
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,          XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX, 
         XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,          XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_ESC,  XXXXXXX,
         XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  TD_SCLN, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, TH_SFT,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT,  TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, TH_SFT,  TH_NAV,           XXXXXXX,          XXXXXXX,          TH_SYM,  TH_CTL,  XXXXXXX, XXXXXXX, XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX 
     ),
 
@@ -49,16 +49,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_LT,   KC_LBRC, KC_LCBR, KC_LPRN, KC_SLSH, XXXXXXX,          XXXXXXX, KC_BSLS, KC_RPRN, KC_RCBR, KC_RBRC, KC_GT,   XXXXXXX, 
         XXXXXXX, KC_GRV,  KC_QUOT, KC_DQUO, KC_DLR,  KC_HASH, XXXXXXX,          XXXXXXX, KC_PLUS, KC_MINS, KC_ASTR, KC_PERC, KC_ESC,  XXXXXXX,
         XXXXXXX, KC_EXLM, KC_TILD, KC_AMPR, KC_PIPE, KC_CIRC,                            KC_AT,   KC_UNDS, KC_QUES, KC_EQL,  KC_COLN, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT, TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS,  TH_NAV,           XXXXXXX,          XXXXXXX,          TH_SYM, TH_CTL,  XXXXXXX, XXXXXXX, XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX 
     ),
 
     [NAV] = LAYOUT_moonlander(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
-        XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LCMD, KC_MPLY, XXXXXXX,          XXXXXXX, TABL,    XXXXXXX, XXXXXXX, KC_TAB,  C_GRV,   XXXXXXX, 
+        XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LCMD, KC_MPLY, XXXXXXX,          XXXXXXX, TABL,    KC_BSPC, KC_ENT,  KC_TAB,  C_GRV,   XXXXXXX, 
         XXXXXXX, OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  G_SPC,   XXXXXXX,          XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, GS_P,    XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_HOME, KC_PGDN, KC_PGUP, KC_END,  G_P,     XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, TH_SFT,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT,  TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, TH_SFT,  TH_NAV,           XXXXXXX,          XXXXXXX,          TH_SYM,  TH_CTL,  XXXXXXX, XXXXXXX, XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX 
     ),
 
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX,          XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, 
         XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,          XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
         XXXXXXX, KC_F11,  KC_F12,  KC_F13,  KC_F14, KC_F15,                              XXXXXXX, XXXXXXX, KC_COMM, KC_DOT,  KC_MINS, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, TH_SFT,  TH_SPC,           XXXXXXX,          XXXXXXX,          TH_ENT,  TH_BSPC, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, TH_SFT,  TH_NAV,           XXXXXXX,          XXXXXXX,          TH_SYM,  TH_CTL,  XXXXXXX, XXXXXXX, XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
     ),
 };
@@ -83,7 +83,7 @@ bool is_oneshot_cancel_key(uint16_t keycode) {
 
 bool is_oneshot_ignored_key(uint16_t keycode) {
     switch (keycode) {
-        case TH_SPC: 
+        case TH_NAV: 
         case OS_SHFT:
         case OS_CTRL:
         case OS_ALT:
